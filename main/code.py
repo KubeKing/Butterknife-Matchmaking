b87cfa9f2bad83d9a580e1a5cc10bd17b4f6021b
Trey <techking802@gmail.com>
2019-02-02 23:50:41 -0500

    Basic Usage

---
+++ main/code.py
@@ -0,0 +1,53 @@
+#Developed by Trey Walker for The Butterknife   
+from __future__ import print_function #Google
+from apiclient import discovery #Google API
+from oauth2client import client #Google API
+from oauth2client import tools #Google API
+from oauth2client.file import Storage #Google API
+import httplib2 #For HTTP Usage
+import requests #For HTTP Usage
+import os #For Local File Usage
+
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'Butterknife-Matchmaking\main\client_secret.json' #Goto Google's API Dev
+APPLICATION_NAME = 'Matchmaker' #Name of Application
+
+def get_credentials():
+    #Gets the credentials to run the Google API
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def getSheet():
+    #Scrapes information off of the quanatiative
+    global mainList
+    mainList = []
+
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1IlHW3GcPT71Q3_k11IFYebdZ7TnqAS6d9Fk7tp0LCcI' #Google Sheet ID
+    rangeName = 'B2:AB' #Range
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+        return('No data found.')
+    else:
+        return(values)
